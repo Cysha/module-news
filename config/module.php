68cb95583d501462b85772585fba78032c9f117d
xLink <xLink@cybershade.org>
2015-03-27 13:01:59 +0000

    removed the homepage override and added config setting for it

--- config/module.php
+++ config/module.php
@@ -3,4 +3,6 @@
 return array(
     'name'        => 'News Module',
     'author'      => 'xLink (Dan Aldridge)',
+
+    'pxcms-index' => 'Cysha\Modules\News\Controllers\Module\NewsController@getNews',
 );
--- routes-module.php
+++ routes-module.php
@@ -7,5 +7,3 @@ Route::group(['prefix' => 'news'], function () use ($namespace) {
     Route::model('newsid', 'Cysha\Modules\News\Models\News');
     Route::get('{newsid}-{slug}', ['as' => 'pxcms.news.view', 'uses' => $namespace.'\NewsController@getNewsById']);
 });
-
-Route::get('/', ['as' => 'pxcms.pages.home', 'uses' => $namespace.'\NewsController@getNews']);
