c86a7b3037d167925e0dcc28edb8f543651a0113
xLink <xLink@cybershade.org>
2015-06-23 14:20:30 +0100

    updated menu to support the new ordering

--- Config/menus.php
+++ Config/menus.php
@@ -3,20 +3,23 @@
 return [
     'backend_sidebar' => [
         'News' => [
-            [
-                'route'         => 'admin.news.manager',
-                'text'          => 'Manage Posts',
-                'icon'          => 'fa-files-o',
-                'order'         => 1,
-                'permission'    => 'manage@news_post',
-                'activePattern' => '\/{backend}\/news\/(\d)\/(.*)'
-            ],
-            [
-                'route'         => 'admin.news.create',
-                'text'          => 'Add News',
-                'icon'          => 'fa-file-text',
-                'order'         => 2,
-                'permission'    => 'manage.create@news_post',
+            'order' => '5',
+            'children' => [
+                [
+                    'route'         => 'admin.news.manager',
+                    'text'          => 'Manage Posts',
+                    'icon'          => 'fa-files-o',
+                    'order'         => 1,
+                    'permission'    => 'manage@news_post',
+                    'activePattern' => '\/{backend}\/news\/(\d)\/(.*)'
+                ],
+                [
+                    'route'         => 'admin.news.create',
+                    'text'          => 'Add News',
+                    'icon'          => 'fa-file-text',
+                    'order'         => 2,
+                    'permission'    => 'manage.create@news_post',
+                ],
             ],
         ],
     ],
