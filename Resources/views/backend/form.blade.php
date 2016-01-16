130f0ad6552ae7df53d3116a99513c7550ab9b0d
xLink <xLink@cybershade.org>
2016-01-16 02:10:48 +0000

    fixed tab index on form

--- Resources/views/backend/form.blade.php
+++ Resources/views/backend/form.blade.php
@@ -3,18 +3,18 @@
     <div class="panel panel-default">
         <div class="panel-heading">Post Details</div>
         <div class="panel-body">
-            {!! Former::text('title')->required() !!}
+            {!! Former::text('title')->required()->tabindex('1') !!}
 
-            {!! Former::text('slug')->prepend('news/')->required() !!}
+            {!! Former::text('slug')->prepend('news/')->required()->tabindex('2') !!}
 
-            {!! Former::text('publish_at')->inlineHelp('<small>Published will be set to submitted date if not present. if you want to specify, YYYY-mm-dd h:i:s</small>') !!}
+            {!! Former::text('publish_at')->inlineHelp('<small>Published will be set to submitted date if not present. if you want to specify, YYYY-mm-dd h:i:s</small>')->tabindex('3') !!}
 
             {!! Former::radios('hide')->radios([
                 'Yes' => ['value' => '1'],
                 'No' => ['value' => '0'],
-            ])->inline()->required() !!}
+            ])->inline()->required()->tabindex('4') !!}
 
-            <button class="btn-labeled btn btn-success pull-right" type="submit">
+            <button class="btn-labeled btn btn-success pull-right" type="submit" tabindex="6">
                 <span class="btn-label"><i class="glyphicon glyphicon-ok"></i></span> Save
             </button>
         </div>
@@ -23,7 +23,7 @@
     <div class="panel panel-default">
         <div class="panel-heading">Post Content</div>
         <div class="panel-body">
-            @include(config('cms.core.app.default-editor'), ['id' => 'content', 'content' => $content])
+            @include(config('cms.core.app.default-editor'), ['id' => 'content', 'content' => $content, 'tabindex' => 5])
         </div>
     </div>
 
